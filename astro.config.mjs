a0b6ca92a42fecc32a978de31ec1459692afa05a
sandbox co.,ltd <info@sndbox.jp>
2026-03-25 08:32:34 +0900

    ｋ

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hotaka-law.hotaka-g.jp/',
+  site: 'https://himorogi.hotaka-g.jp/',
   integrations: [sitemap()],
   // 💡 ViteのCSSプリプロセッサ設定を追加
   vite: {
